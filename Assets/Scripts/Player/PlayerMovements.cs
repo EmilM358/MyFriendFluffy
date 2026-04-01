f7006a3710decfc95280b146f787fc99a1c9701a
EmilM358 <z.rhinestone.eyes@gmail.com>
2026-04-01 13:48:35 -0400

    TemporaryPlayer Prefab Fix

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -51,9 +51,9 @@ public class PlayerMovements : MonoBehaviour
         }
         // ----------- Fall -----------
         velocity.y += gravity * Time.deltaTime;
-        if (!isGrounded && velocity.y < -10f)
+        if (!isGrounded && velocity.y < -5f)
         {
-            velocity.y = -10f;
+            velocity.y = -5f;
         }
 
         // ----------- Player Jump -----------
